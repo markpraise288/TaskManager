5204d2c0093a57601f774fdb37f78c40fcd0bab8
markpraise288 <markpraise288@gmail.com>
2026-03-05 21:49:58 +0000

    Update API_URL to use import.meta.env

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const API_URL = process.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
   const [formData, setFormData] = useState({
     email: "",
     password: "",
