c460986c1622c1fc654950358d48b720f40a7567
markpraise288 <markpraise288@gmail.com>
2026-03-06 13:02:18 +0000

    Remove email transporter import
    Removed email transporter import from auth services.

--- backend/src/modules/auth/auth.services.js
+++ backend/src/modules/auth/auth.services.js
@@ -1,6 +1,5 @@
 const User = require("../../models/user.model");
 const bcrypt = require("bcrypt");
-const transporter = require("../../infrastructure/email/email.services");
 const {
   generateAccessToken,
   generateRefreshToken,
