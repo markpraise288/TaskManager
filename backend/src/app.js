dd4ab76e13a2ff383304e6ae883d92eee667ea73
markpraise288 <markpraise288@gmail.com>
2026-03-06 09:03:22 +0000

    Allow multiple origins for CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+        process.env.FRONTEND_URL,
+        "http://localhost:5173"
+        ],
     credentials: true
 }));
 
