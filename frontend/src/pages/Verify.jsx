d40f5555e8825612caac8b2071549152fb687b04
markpraise288 <markpraise288@gmail.com>
2026-03-06 13:17:30 +0000

    Update Verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,11 +1,1 @@
-import { VerifyForm } from "../components/auth/VerifyForm";
 
-export const Verify = () => {
-
-    return(
-        <div className="flex justify-center items-center w-full">
-            <VerifyForm/>
-        </div>
-        
-    )
-};
