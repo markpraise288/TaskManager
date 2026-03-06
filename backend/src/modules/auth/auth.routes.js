d8bdd0f6b4199f884a1d33be4881be439ebf26d7
markpraise288 <markpraise288@gmail.com>
2026-03-06 12:53:47 +0000

    Remove email verification route from auth routes
    Removed the verifyEmailHandler route from auth.routes.js.

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signupSchema, loginSchema, verifySchema, logoutSchema } = require('../../modules/auth/auth.validate');
-const { signupHandler, verifyEmailHandler, loginHandler, logoutHandler, refreshAccessTokenHandler } = require('./auth.controller');
+const { signupHandler, loginHandler, logoutHandler, refreshAccessTokenHandler } = require('./auth.controller');
 const router = express.Router();
 const authMiddleware = require('../../middlewares/auth.middleware');
 const validate = require('../../middlewares/validate.middleware');
@@ -8,7 +8,6 @@ const validate = require('../../middlewares/validate.middleware');
 router.post('/signup', validate(signupSchema), signupHandler);
 router.post('/login', validate(loginSchema), loginHandler);
 router.post('/logout', validate(logoutSchema), logoutHandler);
-router.post('/verify', validate(verifySchema), verifyEmailHandler);
 router.get('/accessToken', refreshAccessTokenHandler);
 
 module.exports = router;
